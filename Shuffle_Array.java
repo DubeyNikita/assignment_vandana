6bff1cecb5f857b8ffc6d98122abfd6fed7c43bf
DubeyNikita <91541472+DubeyNikita@users.noreply.github.com>
2023-10-13 19:29:36 +0530

    Add files via upload

---
+++ Shuffle_Array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7};
+        List<Integer> list = Arrays.asList(array);
+
+        // Shuffle the list
+        Collections.shuffle(list);
+
+        // Convert the shuffled list back to an array
+        Integer[] shuffledArray = list.toArray(new Integer[0]);
+
+        // Print the shuffled array
+        System.out.println("Shuffled Array: " + Arrays.toString(shuffledArray));
+    }
+}
---
+++ descendind_array.js
@@ -0,0 +1,5 @@
+const arr = [5, 3, 9, 1, 8, 2];
+
+arr.sort((a, b) => b - a);
+
+console.log("Sorted Array in Descending Order:", arr);
